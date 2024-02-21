bde81b0694cd0a0d72aaafcbe3cc8b034837e8d6
joe34674 <joepaul.martin1448@gmail.com>
2024-02-22 01:39:30 +0530

    added websocket server side

--- cmd/chat-server/main.go
+++ cmd/chat-server/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the homepage")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("client device connected")
+
+	err = ws.WriteMessage(1, []byte("hello client device"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
 
 func main() {
-	fmt.Println("first file from ubuntu")
+	fmt.Println("starting main function")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
